ac68a5224220a1a7d3abb5b1c6b7e1c5c7d2bcc9
hitixa-bhuva <hitixa.bhuva@theuniqueitsolution.com>
2025-05-29 16:00:14 +0530

    change base url

--- A_Layout/Header/Header_Partials/A_HeaderMeta.php
+++ A_Layout/Header/Header_Partials/A_HeaderMeta.php
@@ -5,7 +5,7 @@ echo
 <html lang="en">
 
 <head>
-<base href="https://localhost/uniqueitsolution/theuniqueitsolution/">
+<base href="https://theuniqueitsolution.com/demo/">
 
 <meta charset="utf-8" />
 <meta http-equiv="x-ua-compatible" content="ie=edge" />
